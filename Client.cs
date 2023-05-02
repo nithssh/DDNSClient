f0eb8bb1272319520654a0b49397043555a7c122
Nithish Kumar <nithssh@protonmail.com>
2023-05-02 20:44:33 +0530

    Handle network errors better

--- Client.cs
+++ Client.cs
@@ -1,31 +1,27 @@
 ﻿using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace DDNSClient;
 public sealed class Client
 {
     string User, Password, UserAgent, Hostname;
-    string Ip, RetCode;
+    string? Ip;
     readonly ILogger Logger;
     readonly HttpClient HttpClient = new();
+    Result LastResult;
 
     public Client(ILogger<Client> logger, IOptions<DDNSConfig> options)
     {
         Logger = logger;
 
         // unpack the config options
-        User = options.Value.Username; 
+        User = options.Value.Username;
         Password = options.Value.Password;
         UserAgent = "DDNSClient/1.0 " + options.Value.Email;
         Hostname = options.Value.Hostname;
 
         // configure the HTTP client
-        HttpClient.DefaultRequestHeaders.Add("Authorization", "Basic" + Convert.ToBase64String(Encoding.UTF8.GetBytes($"{User}:{Password}")));
+        HttpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "Basic" + Convert.ToBase64String(Encoding.UTF8.GetBytes($"{User}:{Password}")));
         HttpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", UserAgent);
         //HttpClient.DefaultRequestHeaders.UserAgent = userAgent;
     }
@@ -35,60 +31,79 @@ public sealed class Client
         try
         {
             Ip = await GetCurrentIP();
-            PerformDNSUpdate();
-        } catch (HttpRequestException ex)
+            PerformDNSUpdate(); // Force update on init to make sure everything works.
+        }
+        catch (HttpRequestException ex)
         {
             Logger.LogError("HTTP Error: {ExMessage}", ex.Message);
-            // TODO handle network error (not connected)
-            Environment.Exit(2);
+            LastResult = Result.NetworkError;
         }
     }
 
     public async void Update()
     {
-        string ip = await GetCurrentIP();
-        if (ip == Ip)
+        string ip;
+        try
         {
-            Logger.LogInformation("IP unchanged, waiting five minutes.");
+            if (Ip == null && LastResult == Result.NetworkError)
+            {
+                Initialize(); // reforce init, since it didn't occur first time due to network errors
+            }
+            else
+            {
+                ip = await GetCurrentIP();
+                if (ip == Ip)
+                {
+                    Logger.LogInformation("IP unchanged, waiting five minutes.");
+                    LastResult = Result.NoChange;
+                }
+                else
+                {
+                    Ip = ip;
+                    Logger.LogInformation("IP change detected. Updating DNS entry.");
+                    PerformDNSUpdate();
+                }
+            }
+
         }
-        else
+        catch (HttpRequestException ex)
         {
-            Ip = ip;
-            Logger.LogInformation("IP change detected. Updating DNS entry.");
-            PerformDNSUpdate();
+            Logger.LogError("Error updating IP. {ExMessage}", ex.Message);
+            LastResult = Result.NetworkError;
         }
     }
 
     /// <summary>
-    /// Send a Dynamic DNS update, using the Ip field's value.
+    /// Send a Dynamic DNS update, using the Ip field's value, and update the LastResult field.
     /// </summary>
     async void PerformDNSUpdate()
     {
         // Google DNS update URI pattern
         // https: //username:password@domains.google.com/nic/update?hostname=subdomain.yourdomain.com&myip=1.2.3.4
         string reqURI = $"https://{User}:{Password}@domains.google.com/nic/update?hostname={Hostname}&myip={Ip}";
-        RetCode = await HttpClient.GetStringAsync(reqURI);
-        if (!RetCode.StartsWith("good") && !RetCode.StartsWith("nochg"))
+        string retCode = await HttpClient.GetStringAsync(reqURI);
+
+        if (retCode.StartsWith("good"))
         {
-            Logger.LogError("Error with DNS update request: {Error} \nExiting...", RetCode);
-            Environment.Exit(1);
+            Logger.LogInformation("DNS update successful. Return code: ", retCode);
+            LastResult = Result.Success;
+        }
+        else if (retCode.StartsWith("nochg"))
+        {
+            Logger.LogInformation("No change in DNS record. Return code: ", retCode);
+            LastResult = Result.NoChange;
         }
         else
         {
-            Logger.LogInformation("DNS update successful. Return code: ", RetCode);
+            Logger.LogError("Error with DNS update request: {retCode} \nExiting...", retCode);
+            LastResult = Result.Failed;
+            Environment.Exit(2); // Some inherent problem with DNS updates, that must be fixed before retrying.
         }
     }
 
     async Task<string> GetCurrentIP()
     {
         // TODO make ip detection service user configurable
-        /* 
-        // For using checkip service
-        var res = await HttpClient.GetAsync("http://checkip.dyndns.com");
-        res.EnsureSuccessStatusCode();
-        string newIP = await res.Content.ReadAsStringAsync(); 
-        return newIP.Split("IP Address: ")[1].Split("</body>")[0];
-        */
         return await HttpClient.GetStringAsync("https://domains.google.com/checkip");
     }
 }
@@ -99,4 +114,12 @@ public class DDNSConfig
     public string Password { get; set; }
     public string Email { get; set; }
     public string Hostname { get; set; }
+}
+
+enum Result
+{
+    Success,
+    NoChange,
+    Failed,
+    NetworkError
 }
